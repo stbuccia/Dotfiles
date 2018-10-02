78bf6a53cd910dfbac4641d11ec7d793ff430c6e
stbuccia <stbuccia@yahoo.it>
2018-10-02 11:24:51 +0200

    Aggiunto install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#rimuovo i file vecchi
+rm ~/.vimrc
+rm -rf ~/vim
+rm ~/.bashrc
+
+#li ricostruisco facendo dei link simbolici
+ln -s ~/Dotfiles/vim ~/.vim
+ln -s ~/Dotfiles/bashrc ~/.bashrc
+
+#installa vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#installa fzf
+[[ -d $HOME/.fzf ]] || (echo "Installing fzf... " && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install)
