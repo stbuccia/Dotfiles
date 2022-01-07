92e1db86725d55da639bdf50919c560e636d3626
Stefano <stbuccia@gmail.com>
2022-01-07 16:15:16 +0100

    Aggiunti alias con emoji

---
+++ emoji-alias.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+alias 🦜='echo `fc -s`'
+
+alias 🗺='tree'
+
+alias 🔱='sudo su -'
+
+alias ⚓='su stefano'
+
+alias 🧭='__fzf_select__'
+
+alias 🎲='echo $(( ( RANDOM % 6 )  + 1 ))'
+
+alias 🪙='if [[ $(( ( RANDOM % 2 ) )) = 0 ]]; then echo "HEAD"; else echo "CROSS"; fi'
+
+alias 🛶='cd ..'
+
+alias 🍺='rum'
+
+alias 💣='rm -rf *'
+
+alias 🧜='sleep $(( RANDOM % 10 ))'
+
+alias 🏴‍☠️="mpv Music/Wuolters/Wuolters\ -\ L\'ultimo\ goccio\ di\ rum.mp3"
